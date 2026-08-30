8c9f5a057b187baf0334dda3aaa104a0dada1917
Kevin Cui <kevincuigenius@gmail.com>
2026-08-29 22:07:14 -0400

    08292026_Kevin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JoystickOperations.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JoystickOperations.java
@@ -1,5 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
-public class JoystickOperations {
-    //fdsafasfas
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Joystick Operations")
+public class JoystickOperations extends OpMode {
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void loop() {
+
+        double leftPower = -gamepad1.left_stick_y;
+        double rightPower = -gamepad1.right_stick_y;
+
+        if (leftPower > 1.0) {
+            leftPower = 1.0;
+        }
+
+        if (leftPower < -1.0) {
+            leftPower = -1.0;
+        }
+
+        if (rightPower > 1.0) {
+            rightPower = 1.0;
+        }
+
+        if (rightPower < -1.0) {
+            rightPower = -1.0;
+        }
+
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
+    }
 }
