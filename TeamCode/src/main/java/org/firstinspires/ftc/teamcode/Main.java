2ce75b96354b8eafd094d4391bf06533383ef596
Kevin Cui <kevincuigenius@gmail.com>
2026-09-05 20:43:29 -0400

    20260905_Kevin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -9,14 +9,25 @@ public class Main extends OpMode {
     private MecanumDrive mecanumDrive;
     private Intake intake;
 
+    private double endGameStart;
+    private boolean isEndGame;
+
     @Override
     public void init() {
 
         mecanumDrive = new MecanumDrive(hardwareMap);
         intake = new Intake(hardwareMap);
 
+        isEndGame = false;
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+    }
+
+    @Override
+    public void start() {
+
+        endGameStart = getRuntime() + 90;
     }
 
     @Override
@@ -29,11 +40,17 @@ public class Main extends OpMode {
         mecanumDrive.mecunamDrive(y, x, rx);
         intake.intake(gamepad1);
 
+        if(getRuntime() >= endGameStart && !isEndGame) {
+            gamepad1.rumbleBlips(3);
+            isEndGame = true;
+        }
+
         telemetry.addData("Front Left Power", mecanumDrive.getLeftFrontPower());
         telemetry.addData("Front Right Power", mecanumDrive.getRightFrontPower());
         telemetry.addData("Back Left Power", mecanumDrive.getLeftBackPower());
         telemetry.addData("Back Right Power", mecanumDrive.getRightBackPower());
         telemetry.addData("Intake Power", intake.getIntakePower());
+        telemetry.addData("Is it Endgame", isEndGame);
         telemetry.update();
     }
 }
