361ad61f4f21f85419f1816f68583f0e9aa46c71
Vihaan Nakirikanti <vihaan.nakirikanti@gmail.com>
2026-09-13 19:48:17 -0400

    20260913 Added Limelight Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Limelight.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Limelight extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
