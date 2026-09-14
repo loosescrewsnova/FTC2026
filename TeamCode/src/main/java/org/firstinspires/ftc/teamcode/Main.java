72276e7fed16244c9eb82206632cbc14f297d282
Kevin Cui <kevincuigenius@gmail.com>
2026-09-13 20:10:32 -0400

    20260913_Kevin_ShooterFinished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -35,7 +35,6 @@ public class Main extends OpMode {
 
     @Override
     public void start() {
-
         endGameStart = getRuntime() + 90;
     }
 
@@ -52,6 +51,7 @@ public class Main extends OpMode {
         mecanumDrive.updateOdometry(imuOrthogonal.getIMU());
 
         intake.intake(gamepad1);
+        shooter.shooter(gamepad1);
 
         if (gamepad1.y) {
             imuOrthogonal.resetYaw();
@@ -86,6 +86,11 @@ public class Main extends OpMode {
         telemetry.addData(
                 "Intake Power",
                 intake.getIntakePower()
+        );
+
+        telemetry.addData(
+                "Shooter Power",
+                shooter.getShooterSpeed()
         );
 
         telemetry.addData(
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -3,37 +3,57 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Shooter {
 
-    private double buttonStartTime;
+    private final DcMotor shooterMotor;
+
+    private final ElapsedTime buttonTimer = new ElapsedTime();
 
     private boolean buttonWasPressed = false;
 
+    private double shooterSpeed = 0;
+
     public Shooter(HardwareMap hardwareMap) {
 
-        DcMotor shooterMotor = hardwareMap.get(DcMotor.class, "shooterMotor");
+        shooterMotor = hardwareMap.get(DcMotor.class, "shooterMotor");
     }
 
     public void shooter(Gamepad gamepad) {
 
-        double shooterSpeed = 1;
+        // Button was just pressed
+        if (gamepad.dpad_left && !buttonWasPressed) {
 
-        if(gamepad.dpad_left && !buttonWasPressed) {
+            buttonTimer.reset();
+            buttonWasPressed = true;
+        }
 
+        // Button is being held
+        if (gamepad.dpad_left) {
 
+            double heldTime = buttonTimer.seconds();
+
+            // Increase power based on how long the button is held
+            shooterSpeed = heldTime * 0.2;
+
+            // Maximum power is 1.0
+            shooterSpeed = Math.min(shooterSpeed, 1.0);
+
+            shooterMotor.setPower(shooterSpeed);
+        }
+
+        // Button was released
+        if (!gamepad.dpad_left) {
+
+            shooterSpeed = 0;
+            shooterMotor.setPower(0);
+
+            buttonWasPressed = false;
         }
     }
 
-    public boolean isButtonWasPressed() {
-        return buttonWasPressed;
-    }
-
-    public void setButtonWasPressed(boolean buttonWasPressed) {
-        this.buttonWasPressed = buttonWasPressed;
-    }
-
-    public double getShooterSpeed(double shooterSpeed) {
+    public double getShooterSpeed() {
         return shooterSpeed;
     }
 }
