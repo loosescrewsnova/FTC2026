e87942c555c41a59c45c2f308066b28fd9e2dcf0
Vihaan Nakirikanti <vihaan.nakirikanti@gmail.com>
2026-09-15 18:40:56 -0400

    20260915 vihaan started to work on the limelight, did basic things, but need to work more on other parts before fully coding.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Limelight.java
@@ -1,19 +1,28 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
 
-@TeleOp
+
 public class Limelight extends OpMode {
-
+    private Limelight3A limelight;
+    private IMU imu;
     @Override
-    public void init() {
+    public void init(){
+        limelight= hardwareMap.get(Limelight3A.class, "Limelight");
+        imu = hardwareMap.get(IMU.class,"imu");
+
+        limelight.pipelineSwitch(12); //change it based on the april tag number that occurs
 
     }
-
     @Override
-    public void loop() {
+    public void start(){
+
+    }
+    @Override
+    public void loop(){
 
     }
 }
